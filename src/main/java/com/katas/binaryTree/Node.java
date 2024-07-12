11af169e19d2047c01490e77247ef920de823705
Mats Odolphij <matsodolphij@gmail.com>
2024-07-12 15:47:15 +0200

    added all code from the other repo which is now deprecated

---
+++ src/main/java/com/katas/binaryTree/Node.java
@@ -0,0 +1,20 @@
+package com.katas.binaryTree;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class Node {
+
+    int value;
+    @Setter
+    Node left;
+    @Setter
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
---
+++ src/test/java/com/research/katas/depthFirstSearch/DepthFirstSearchTest.java
@@ -0,0 +1,40 @@
+package com.research.katas.depthFirstSearch;
+
+import org.junit.jupiter.api.Test;
+
+import com.katas.binaryTree.BinaryTree;
+import com.katas.depthFirstSearch.DepthFirstSearch;
+
+public class DepthFirstSearchTest {
+
+    DepthFirstSearch depthFirstSearch = new DepthFirstSearch();
+
+    @Test
+    void inOrder() {
+        depthFirstSearch.traverseInOrder(getBinaryTree().getRoot());
+    }
+    @Test
+    void postOrder() {
+        depthFirstSearch.traversePostOrder(getBinaryTree().getRoot());
+    }
+
+    @Test
+    void preOrder() {
+        depthFirstSearch.traversePreOrder(getBinaryTree().getRoot());
+    }
+
+    private BinaryTree getBinaryTree() {
+        BinaryTree root = new BinaryTree();
+        root.add(3);
+        root.add(4);
+        root.add(7);
+        root.add(1);
+        root.add(2);
+        root.add(5);
+        root.add(6);
+        root.add(8);
+        root.add(9);
+        root.add(10);
+        return root;
+    }
+}
